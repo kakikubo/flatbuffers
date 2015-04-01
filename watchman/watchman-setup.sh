2d4001e4796b139e392445f81da1666a598dbdcf
Kiyoto SUZUKI <kiyoto.suzuki@gree.net>
2015-04-01 17:23:26 +0900

    change for kms/asset/hook to kms/took/

--- watchman/watchman-setup.sh
+++ watchman/watchman-setup.sh
@@ -4,17 +4,17 @@ watchman=/usr/local/bin/watchman
 jq=/usr/loca/bin/jq
 
 cd `dirname $0`/..
-script_dir=`pwd`
+tool_dir=`pwd`
 
 root_dir=$1
-[ -n "$root_dir" ] || root_dir=$script_dir
+[ -n "$root_dir" ] || root_dir=$tool_dir/../kms_master_asset
 cd $root_dir || exit $?
 root_dir=`pwd`
 echo "watch root dir: $root_dir"
 
-for template in $script_dir/hook/*.json.template; do
+for template in $tool_dir/watchman/*.json.template; do
   json=`echo $template | sed -e 's/\.template$//'`
-  cat $template | sed -e "s!__ROOT_DIR__!$root_dir!g" | sed -e "s!__SCRIPT_DIR__!$script_dir!g" > $json
+  cat $template | sed -e "s!__ROOT_DIR__!$root_dir!g" | sed -e "s!__TOOL_DIR__!$tool_dir!g" > $json
   path=`cat $json | jq -r '.[1]'`
   name=`cat $json | jq -r '.[2]["name"]'`
 
