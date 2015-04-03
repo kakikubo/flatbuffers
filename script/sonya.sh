ba58339eb363d71f756117edbc3efd2a94e7a7b7
Kiyoto SUZUKI <kiyoto.suzuki@gree.net>
2015-04-03 15:26:19 +0900

    add usage

--- script/sonya.sh
+++ script/sonya.sh
@@ -5,5 +5,15 @@ sonya_chan_url=http://skail.gree-dev.net:4979/send_chat
 #chat_id=27838766  # KMSエンジニア
 chat_id=31118592  # KMSビルド
 
-curl --silent -F rid=$chat_id -F text="[info][title]$1[/title]`hostname`:$2[code]`tail $2`[/code]$3[/info]" $sonya_chan_url || exit $?
+title=$1
+log_file=$2
+reference_url=$3
+[ -n "$4" ] && chat_id=$4
+if [ "$#" -lt 2 ]; then
+  echo "$0 <title> <log_file> [<reference_url> [<chat_id>]]"
+  echo "Chatwork Chat ID: $chat_id"
+  exit 1
+fi
+
+curl --silent -F rid=$chat_id -F text="[info][title]$title[/title]`hostname`:$log_file[code]`tail $log_file`[/code]$reference_url[/info]" $sonya_chan_url || exit $?
 exit 0
