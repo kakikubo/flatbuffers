dca7216571b25d6a07f71e8c08316632dc6cb0c7
Kiyoto SUZUKI <kiyoto.suzuki@gree.net>
2015-06-11 16:55:36 +0900

    support multi users for box sync waiting

--- watchman/watchman-callback.sh
+++ watchman/watchman-callback.sh
@@ -14,27 +14,34 @@ echo "\n\n\n\n\n---- $WATCHMAN_TRIGGER $LOGNAME@$WATCHMAN_ROOT (`date`)"
 read json
 echo $json | jq '.'
 
+function get_names() {
+  echo `cat $1 | jq -r '.[] | (if .exists and .new then "A" elif .exists then "U" else "D" end) + " " + .["name"]' | tr '\n' '+'`
+}
+
 # merge changed files log
-head_file=/tmp/watchman-callback.head.json
-current_file=/tmp/watchman-callback.current.json
-param_file=/tmp/watchman-callback.$$.json
-sonya_file=/tmp/watchman-callback.sonya.$$.log
+head_file=/tmp/watchman-callback.$target.head.json
+current_file=/tmp/watchman-callback.$target.current.json
+sonya_file=/tmp/watchman-callback.$target.sonya.log
 
 touch $head_file || exit $?
 echo "`cat $head_file` $json" | jq -s '.[0] + .[1]' > $current_file || exit $?
-names=`cat $current_file | jq -r '.[] | (if .exists and .new then "A" elif .exists then "U" else "D" end) + " " + .["name"]' | tr '\n' '+'`
 
 # check box sync is completed
 if ! $tool_dir/script/box-sync-completed.sh; then
   mv $current_file $head_file
-  echo $names > $sonya_file
+  get_names $current_file > $sonya_file
   $tool_dir/script/sonya.sh "(yawn) watchman '$WATCHMAN_TRIGGER' is waiting for Box Sync" $kms_dev_url $sonya_file || exit $?
   exit 0
 fi
-rm -f $head_file || exit $?
 
 # launch jenkins job
-echo "{\"parameter\": [{\"name\": \"OPT_TARGET\", \"value\": \"$target\"}, {\"name\": \"OPT_FILES\", \"value\": \"$names\"}]}" > $param_file
-cat $param_file
-curl $jenkins_url -X POST --form json=@$param_file
-exit $?
+for file in `ls -t /tmp/watchman-callback.*.head.json`; do
+  target=`basename $file | sed -e 's/^watchman-callback.//' | sed -e 's/.head.json$//'`
+  names=`get_names $file`
+  param_file=/tmp/watchman-callback.$target.$$.json
+  echo "{\"parameter\": [{\"name\": \"OPT_TARGET\", \"value\": \"$target\"}, {\"name\": \"OPT_FILES\", \"value\": \"$names\"}]}" > $param_file
+  cat $param_file
+  curl $jenkins_url -X POST --form json=@$param_file || exit $?
+  rm -f $file || exit $?
+done
+exit 0
