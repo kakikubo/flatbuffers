71f28cf83cf2caa4d5358bb749ad30048307fd81
kms.jenkins <kms.jenkins@G-PC-6643.local>
2015-06-30 19:41:30 +0900

    modify error message

--- script/verify_user_json.py
+++ script/verify_user_json.py
@@ -11,17 +11,29 @@ import logging
 from logging import info, warning, error
 from collections import OrderedDict
 from glob import glob
+from subprocess import check_output
 
 def verify_user_json(src_dir):
+    if not os.path.isdir(src_dir):
+        raise Exception("not dir: %s" % src_dir)
+
     user_data = OrderedDict()
     for json_path in glob("%s/*.json" % src_dir):
         key = re.sub('.json$', '', os.path.basename(json_path))
         info("verify user data: %s" % key)
         with open(json_path, 'r') as f:
-            user_data[key] = json.load(f, object_pairs_hook=OrderedDict)
+            try:
+                user_data[key] = json.load(f, object_pairs_hook=OrderedDict)
+            except ValueError, e:
+                print("\n---------------------------")
+                print("    USER DATA ERROR      ")
+                print("---------------------------")
+                print(json_path)
+                print(e)
+                print(check_output(["jq", ".", json_path]))
+                raise 
 
     # TODO check user data
-
     return True
 
 # ---
