b258865a8ebf02e2518aeda87c80fcc962666a39
kms.jenkins <kms.jenkins@G-PC-6643.local>
2015-07-07 21:49:53 +0900

    fix typo

--- script/fbs2class.py
+++ script/fbs2class.py
@@ -219,7 +219,7 @@ def generate_classes(namespace=None, with_json=True, with_msgpack=True, with_fbs
                     s += "  }\n"
                 else:
                     s += "  void assign" + upper_camel_case(item_name) + "(int pos, const " + item["cpp_type"] + "& value, const " + item["cpp_type"] + "& defaultValue) {\n"
-                    s += "    if (_" + item_name + ".capacity() >= pos) {\n"
+                    s += "    if (_" + item_name + ".capacity() <= pos) {\n"
                     s += "      _" + item_name + ".resize(pos + 1, defaultValue);\n"
                     s += "    }\n"
                     s += "    _" + item_name + ".assign(pos, value);\n"
