ca2158b974a4f7a9b743cecc00d9790cfec6dd7c
Ce Zheng <ce.zheng@gree.net>
2015-11-13 23:38:48 +0900

    generate webviews.json in build_dir then sync to main_dir

--- script/update_webviews.py
+++ script/update_webviews.py
@@ -16,8 +16,9 @@ from subprocess import check_call
 class WebViewUpdater(object):
     all_enviroments = ['dev', 'qa', 'release']
     sync_options = ['--exclude', '*.DS_Store', '--exclude', '*.git', '--delete']
-    def __init__(self, root_dir, env='all'):
+    def __init__(self, root_dir, build_dir, env='all'):
         self.root_dir = root_dir
+        self.build_dir = build_dir
         self.envs = [env]
         if self.envs == ['all']:
             self.envs = self.all_enviroments
@@ -27,15 +28,27 @@ class WebViewUpdater(object):
     def generate_json(self, platform):
         for env in self.envs:
             src_path = join(self.root_dir, "webview", env, platform)
+            dst_path = join(self.build_dir, "webview", env, platform)
             if not isdir(src_path):
                 info("%s does not exists." % src_path)
                 continue
             html_files = self.list_html_files(src_path, "kms://")
-            dst_file = join(src_path, "webviews.json")
+            if not isdir(dst_path):
+                os.makedirs(dst_path)
+            dst_file = join(dst_path, "webviews.json")
             info("Generating webview json list for platform %s: %s", platform, dst_file)
             with open(dst_file, 'w') as fout:
                 json.dump(html_files, fout, indent=2)
             os.chmod(dst_file, 0664)
+
+    def sync_with_root(self):
+        rsync = ['rsync', '-ac', '--exclude', '*.DS_Store']
+        for env in self.envs:
+            src_path = join(self.build_dir, "webview", env, '')
+            dst_path = join(self.root_dir, "webview", env, '')
+            cmdline = rsync + [src_path, dst_path]
+            debug(' '.join(cmdline))
+            check_call(cmdline)
 
 
     def list_html_files(self, path, url_prefix):
@@ -97,17 +110,22 @@ examples:
   update then deploy for all environments
       $ ./script/update_webviews.py --root-dir ${ROOT_DIR} update_deploy
         """)
-    parser.add_argument('command',         help = 'command (update|deploy|update_deploy)')
-    parser.add_argument('--environment',      help = 'environment (all|dev|qa|release). default: all', default='all')
-    parser.add_argument('--root-dir',      help = 'root directory (webview folder\'s parent)', required=True)
-    parser.add_argument('--cdn-dir',       help = 'cdn directory to deploy. default: /var/www/cdn', default='/var/www/cdn')
-    parser.add_argument('--log-level',     help = 'log level (WARNING|INFO|DEBUG). default: INFO', default='INFO')
+    parser.add_argument('command',          help = 'command (update|deploy|update_deploy)')
+    parser.add_argument('--environment',    help = 'environment (all|dev|qa|release). default: all', default='all')
+    parser.add_argument('--root-dir',       help = 'root directory (webview folder\'s parent)', required=True)
+    parser.add_argument('--build-dir',      help = 'build directory', required=True)
+    parser.add_argument('--cdn-dir',        help = 'cdn directory to deploy. default: /var/www/cdn', default='/var/www/cdn')
+    parser.add_argument('--skip-sync-root', help = 'skip syncing build_dir contents to root_dir(ignored if command is `deploy`) (0|1)', default=0, type=int)
+    parser.add_argument('--log-level',      help = 'log level (WARNING|INFO|DEBUG). default: INFO', default='INFO')
+
     args = parser.parse_args()
     logging.basicConfig(level=args.log_level, format='%(asctime)-15s %(levelname)s %(message)s')
-    updater = WebViewUpdater(args.root_dir, args.environment)
+    updater = WebViewUpdater(args.root_dir, args.build_dir, args.environment)
     if args.command in ('update', 'update_deploy'):
         updater.generate_json('ios')
         updater.generate_json('android')
+        if not args.skip_sync_root:
+            updater.sync_with_root()
     if args.command in ('deploy', 'update_deploy'):
         success = updater.deploy_dev_cdn(args.cdn_dir)
         success = updater.deploy_s3_cdn() and success
