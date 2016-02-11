16857e3167faba2e9e4c317c725386488ab18c4e
Kiyoto SUZUKI <kiyoto.suzuki@gree.net>
2016-02-11 21:40:56 +0900

    add chatwork_to.sh

---
+++ script/chatwork_to.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+user=$1
+asset_dir=$2
+[ -d "$asset_dir" ] || asset_dir=~/box/kms_master_asset/
+[ -d "$asset_dir" ] || asset_dir=~jenkins/box/kms_master_asset/
+[ -d "$asset_dir" ] || asset_dir=~kms.jenkins/box/kms_master_asset/
+
+jq -r ".\"$1\"" $asset_dir/manifests/chatwork-users.json
+exit $?
