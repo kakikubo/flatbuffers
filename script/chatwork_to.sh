355a70adad27bc215c66589e7680b691c490569f
Gree WFS KaMaSu Developer <kms-dev@wrightflyer.net>
2016-02-11 21:45:15 +0900

    fix to suppress null

--- script/chatwork_to.sh
+++ script/chatwork_to.sh
@@ -6,5 +6,5 @@ asset_dir=$2
 [ -d "$asset_dir" ] || asset_dir=~jenkins/box/kms_master_asset/
 [ -d "$asset_dir" ] || asset_dir=~kms.jenkins/box/kms_master_asset/
 
-jq -r ".\"$1\"" $asset_dir/manifests/chatwork-users.json
+jq -r ".\"$1\"" $asset_dir/manifests/chatwork-users.json | grep -v null
 exit $?
