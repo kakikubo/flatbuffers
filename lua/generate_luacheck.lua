b8351d3244929d91d932cb6646b6e5aa17c71382
Toshifumi Watanabe <toshifumi.watanabe@gree.net>
2016-02-22 14:10:06 +0900

    add a script to generate .luacheckrc

---
+++ lua/generate_luacheck.lua
@@ -0,0 +1,63 @@
+
+if arg[1] == nil then
+    print("Usage:")
+    print("lua generate_config.lua (path_to_kms_client_repo)")
+    print("")
+    os.exit(1)
+end
+
+package.path=package.path..string.format(";%s/?.lua", arg[1])
+require('lfs')
+
+local Main = {}
+
+Main.repo = arg[1]
+
+Main.lib_paths = {
+    'Resources/lua_foundation',
+    'asset/lua/foundation',
+}
+
+Main.find_modules = function()
+    local list = {}
+    for x, path in pairs(Main.lib_paths) do
+        for file in lfs.dir(string.format('%s/%s', Main.repo, path)) do
+            if string.find(file, '([-_%w]+.lua)') then
+                table.insert(list, string.format('%s/%s', path, string.sub(file, 1, -5)))
+            end
+        end
+    end
+    return list
+end
+
+Main.find_globals = function()
+    local orig = {}
+    local dest = {}
+    for k, v in pairs(_G) do
+        orig[k] = v
+    end
+    for k, v in pairs(Main:find_modules()) do
+        require(v)
+    end
+    for k, v in pairs(_G) do
+        if orig[k] == nil then
+            table.insert(dest, k)
+        end
+    end
+    table.sort(dest)
+    return dest
+end
+
+Main.execute = function()
+    local variables = Main:find_globals()
+
+    print "unused_args=false\n"
+    print "globals = {"
+    for k, v in pairs(variables) do
+        print(string.format('    "%s",', v)) end
+    print '    "KMS"'
+    print "}"
+end
+
+Main.execute()
+
