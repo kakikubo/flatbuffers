18155cf04636c59089647634f0a36cef2c4578c5
Kiyoto SUZUKI <kiyoto.suzuki@gree.net>
2016-03-04 01:47:36 +0900

    support to cast default value

--- script/fbs2class.py
+++ script/fbs2class.py
@@ -159,6 +159,22 @@ def parse_table(line, i):
                         is_hash_key = True
                     elif kk == 'key':
                         is_range_key = True
+    if type == None:
+      raise Exception("failed to parse line: %d '%s'" % (i+1, line))
+
+    if default_value != None:
+        if is_vector:
+            default_value = json.loads(default_value, object_pairs_hook=OrderedDict)
+        elif type == 'string':
+            default_value = str(default_value)
+        elif type in ('int', 'long'):
+            default_value = int(default_value)
+        elif type in ('float', 'double'):
+            default_value = float(default_value)
+        elif type in ('bool'):
+            default_value = bool(default_value)
+        else:
+            raise Exception("invalid default value '%s': %d '%s'" % (default_value, i+1, line))
 
     item = OrderedDict()
     item['name'] = name
@@ -168,8 +184,6 @@ def parse_table(line, i):
     item['is_hash_key'] = is_hash_key
     item['is_range_key'] = is_range_key
     item['attribute'] = attribute
-    if type == None:
-      raise Exception("failed to parse line: %d '%s'" % (i+1, line))
 
     table_name = next(reversed(fbs_data))
     fbs_data[table_name][name] = item
@@ -303,19 +317,18 @@ def generate_classes(namespace=None, with_json=True, with_msgpack=True, with_fbs
         for item_name, item in table.iteritems():
             item_type = item["item_type"]
             default_value = ""
-            if item["attribute"] and item["attribute"].has_key("default"):
-                value = item["attribute"].has_key("default")
+            if item["default_value"] != None:
                 if item_type in ('string'):
-                    default_value = '"'+value+'"';
+                    default_value = '"'+item["default_value"]+'"';
                 elif item_type in ('bool'):
-                    default_value = "true" if value else  "false";
+                    default_value = "true" if item["default_value"] else  "false";
                 else:
-                    default_value = value;
+                    default_value = item["default_value"];
 
             if not item["is_default_type"] and not item["is_vector"]:
-                inits.append("  _" + item_name + "(std::make_shared<" + item["item_type"] + " >("+default_value+")),\n")
-            elif default_value:
-                inits.append("  _" + item_name + "(" + default_value + "),\n")
+                inits.append("  _" + item_name + "(std::make_shared<" + item["item_type"] + " >(" + str(default_value) + ")),\n")
+            elif default_value != None:
+                inits.append("  _" + item_name + "(" + str(default_value) + "),\n")
         s += "\n\n\n"
         s += "// " + table_name + "\n"
         s += "" + table_name + "::" + table_name + "() : \n" + "".join(inits) + "  __timestamp(-1),\n  __dirty(true) {}\n"
@@ -959,7 +972,7 @@ def generate_schema():
             s['type']      = item['item_type']
             s['attribute'] = item['attribute']
             s['is_vector'] = True if item['is_vector'] else False
-            if item['default_value']:
+            if item['default_value'] != None:
                 s['default_value'] = item['default_value']
             schemas[table_type].append(s)
 
