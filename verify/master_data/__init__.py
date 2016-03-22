f24049fbc3c3ae1ce5da58f0cb51448f57c80141
Kiyoto SUZUKI <kiyoto.suzuki@gree.net>
2016-03-23 00:16:24 +0900

    fix to empty __init__.py

--- verify/master_data/__init__.py
+++ verify/master_data/__init__.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-from master_data.base import Base
-from master_data.enemyPlacement import enemyPlacement
- 
 __author__  = 'kiyoto.suzuki'
 __version__ = '0.0.1'
 __license__ = 'MIT'
