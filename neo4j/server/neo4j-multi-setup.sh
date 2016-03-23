266481d991e377568c6b2e07c57e779f7b205444
Gree WFS KaMaSu Developer <kms-dev@wrightflyer.net>
2016-03-23 23:47:37 +0900

    support neo4j 2.3.x

--- neo4j/server/neo4j-multi-setup.sh
+++ neo4j/server/neo4j-multi-setup.sh
@@ -6,7 +6,7 @@ if [ -z "$user_name" -o -z "$port" ]; then
   echo "Usage: $0 <user.name> <port>"
   exit 1
 fi
-NEO4J_ROOT=/usr/local/Cellar/neo4j/2.3.2/libexec
+NEO4J_ROOT=`find /usr/local/Cellar/neo4j/ -name libexec -type d`
 NEO4J_HOME=${NEO4J_MULTI_ROOT:-~/neo4j}/$user_name
 NEO4J_PORT=$port
 
@@ -26,7 +26,7 @@ for dir in bin lib system; do
 done
 
 # environment configuration file
-env_json=/$NEO4J_HOME/neo4j_env.json
+env_json=$NEO4J_HOME/neo4j_env.json
 $template_bin $env_template_json "{\"NEO4J_HOME\": \"$NEO4J_HOME\", \"NEO4J_ROOT\": \"$NEO4J_ROOT\", \"port\": $NEO4J_PORT}" > $env_json
 
 # neo4j conf
